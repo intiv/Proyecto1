93df51f03bcb9b8bb3645853db730e937822cead
intiv <intiv@unitec.edu>
2016-05-17 23:39:20 -0600

    Clase carta

--- carta.cpp
+++ carta.cpp
@@ -4,42 +4,28 @@
 using std::string;
 
 carta::carta(){
-		
+
 }
 
-carta::carta(string symbol,unsigned int value, string colour, char letter){
-	setValor(value);
-	setSimbolo(symbol);
-	setColor(colour);
-	setLetra(letter);
+carta::carta(string symbol, char letter){
+	this->simbolo=symbol;
+	this->letra=letter;
 }
-
-void carta::setValor(unsigned int valor){
-	this->valor=valor;
+/*
+carta::carta(string symbol, string colour, int numero){
+	set
 }
-
+*/
 void carta::setSimbolo(string simbolo){
 	this->simbolo=simbolo;
-}
-
-void carta::setColor(string color){
-	this->color=color;
 }
 
 void carta::setLetra(char letra){
 	this->letra=letra;
 }
 
-unsigned int carta::getValor(){
-	return this->valor;
-}
-
 string carta::getSimbolo(){
 	return this->simbolo;
-}
-
-string carta::getColor(){
-	return this->color;
 }
 
 char carta::getLetra(){
--- carta.h
+++ carta.h
@@ -6,23 +6,17 @@
 using std::string;
 class carta{
 	public:
+		
 		carta();
-		carta(string,unsigned int,string,char);
+		carta(string,char);
+		
+		string getSimbolo();
+		void setSimbolo(string);
+		void setLetra(char);
+		char getLetra();
 	private:
 		string simbolo;
-		unsigned int valor;
-		string color;
 		char letra;
-	
-		void setSimbolo(string);
-		void setValor(unsigned int);
-		void setColor(string);
-		void setLetra(char);
-
-		string getSimbolo();
-		unsigned int getValor();
-		string getColor();
-		char getLetra();
 };
 
 
